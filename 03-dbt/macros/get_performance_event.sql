b3d5797dc5c76e915be4f5675a34a9d3338826cf
ilya.sinev@ruhr-uni-bochum.de <ilya.sinev@ruhr-uni-bochum.de>
2022-04-20 14:28:11 +0000

    Model development #1

---
+++ 03-dbt/macros/get_performance_event.sql
@@ -0,0 +1,18 @@
+{#
+    This macro returns the type of the PERFORMANCE_EVENT
+#}
+
+{% macro get_performance_event(PERFORMANCE_EVENT_CODE) -%}
+
+    case {{ PERFORMANCE_EVENT_CODE }}
+        when 'A' then 'Delay'
+        when 'M' then 'Delay'
+        when 'C' then 'Full cancelletion'
+        when 'D' then 'diversion'
+        when 'F' then 'Failure to stop'
+        when 'O' then 'Part cancellation'
+        when 'P' then 'Part cancellation'
+        when 'S' then 'Scheduled cancellation'
+    end
+
+{%- endmacro %}
