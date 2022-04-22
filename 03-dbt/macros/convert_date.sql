c24ab9324b05f423a8d331fef621807622b57b24
IliaSinev <33204500+IliaSinev@users.noreply.github.com>
2022-04-22 17:03:09 +0200

    Added conversion macros, updated staging script

---
+++ 03-dbt/macros/convert_date.sql
@@ -0,0 +1,14 @@
+{#
+    This macro detects date values stored as string and converts in to DATE 
+#}
+
+{% macro convert_date(datestring) -%}
+
+    CASE 
+        WHEN datestring LIKE "__.%" THEN parse_date("%d.%m.%y", LEFT(datestring, 10))
+        WHEN datestring LIKE "__/%" THEN parse_date("%d/%m/%Y", LEFT(datestring, 10))
+        WHEN datestring LIKE "__-%" THEN parse_date("%d-%b-%Y", LEFT(datestring, 11))
+        ELSE NULL
+    END
+
+{%- endmacro %}
