2baed7b1dbd448c5900736e7b428898602264420
IliaSinev <sinevi@gmial.com>
2022-04-24 00:58:22 +0200

    added dim_date

--- 03-dbt/models/core/dim_stanox_locations.sql
+++ 03-dbt/models/core/dim_stanox_locations.sql
@@ -50,10 +50,25 @@ locations AS
     WHERE Latitude is not null AND Longitude is not null
 )
 SELECT
-    
-FROM {{ source('core', 'dim_Stanox') }} AS stanox
-left outer join {{ref('Stanox-Locations')}} AS loc
-on stanox.STANOX_NO = attr.TLC
+     st.STANOX_NO
+    ,st.FULL_NAME
+    ,st.CRS_CODE
+    ,st.Route_Description
+    ,COALESCE(att.Region, 'NA') AS Region
+    ,COALESCE(att.Local_authority_district_unitary, 'NA') AS Local_authority_district_unitary
+    ,COALESCE(att.Local_authority_county_unitary, 'NA') AS Local_authority_county_unitary
+    ,COALESCE(att.Constituency, 'NA') AS Constituency
+    ,COALESCE(att.ITL2_subregion, 'NA') AS ITL2_subregion
+    ,COALESCE(att.ITL2_subregion_code, 'NA') AS ITL2_subregion_code
+    ,COALESCE(att.Station_group, 'NA') AS Station_group
+    ,COALESCE(att.Network_Rail_Region, 'NA') AS Network_Rail_Region
+    ,loc.Latitude
+    ,loc.Longitude
+FROM stanox_upd AS st
+LEFT OUTER JOIN attributes AS att
+ON st.CRS_CODE = attr.TLC
+LEFT OUTER JOIN lacations AS loc
+ON st.STANOX_NO = loc.Stanox
 
 
 
